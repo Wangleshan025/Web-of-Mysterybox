df39dd0c134bf6679f07ae42dae34e0805648cbd
wangleshan <2972705336@qq.com>
2025-08-01 00:57:11 +0800

    背景与部分配置

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import router from './router';
+import App from './App.vue'
+import 'font-awesome/css/font-awesome.min.css';
+
+createApp(App)
+.use(router)
+.mount('#app');
